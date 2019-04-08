9fd13a05695906eadbbe671b301d925c9f71959c
luciferche <matovicluka@gmail.com>
2019-04-08 17:03:56 +0100

    mock api added

--- mainAjax.js
+++ mainAjax.js
@@ -1,18 +1,23 @@
-/const urlBase = 'https://pokeapi.co/api/v2/';
+//const urlBase = 'https://pokeapi.co/api/v2/';
+const urlBase = 'https://5ca7ef1b8e58df0014603a1e.mockapi.io/api/';
 
 var resultSpan = document.getElementById('content');
+var taskList = document.getElementById('taskList');
+var httpRequest; 
+function getTasks () {
+    // var url = document.getElementById('inputUrl').value;
 
-getUsers
-getJokes
-function get () {
-    var url = document.getElementById('inputUrl').value;
-
-    getAjax(url)
+    getAjax('tasks');
 }
 
-function post (params) {
-    var url = document.getElementById('inputUrl').value;
-    postAjax(url, params);
+function postTask (params) {
+    var name = document.getElementById('inputUTaskName').value;
+    var description = document.getElementById('inputUTaskDescription').value;
+    var task = {
+        name: name,
+        description: description
+    }
+    postAjax(url, task);
 }
 
 
@@ -20,11 +25,11 @@ function getAjax(endpoint) {
 
     var url = urlBase + endpoint;
     console.log('ajax GET - ' + url);
-    var httpRequest = new XMLHttpRequest();
+    httpRequest = new XMLHttpRequest();
 
     if (!httpRequest) {
         alert('Giving up :( Cannot create an XMLHTTP instance');
-    r   return false;
+        return false;
     }
     httpRequest.onreadystatechange = getCallback;
     httpRequest.open('GET', url);
@@ -36,7 +41,9 @@ function getCallback() {
         console.log('request done - status ', httpRequest.status);
         if (httpRequest.status === 200) {
             console.log(httpRequest.responseText);
-            resultSpan.innerHTML = httpRequest.responseText;
+            var tasks = JSON.parse(httpRequest.responseText);
+            console.log('tasks, ', tasks);
+            showTasks(tasks);
             return httpRequest.responseText;
         } else {
             alert('There was a problem with the request.');
@@ -46,25 +53,45 @@ function getCallback() {
         console.log('request not done ', httpRequest.readyState);
     }
 }
+function showTasks(tasks) {
+    taskList.innerHTML = '';
+    resultSpan.innerHTML = httpRequest.responseText;
+    for(let i in tasks) {
+        addTaskElement(tasks[i]);
+    }
+}
+
+function addTaskElement(task) {
+    var element = document.createElement('div');
+    element.className ='task-item';
+    var text = document.createTextNode(task.name + '-');
+    text.className = 'task-name';
+    var desc = document.createTextNode(task.description);
+    desc.className = 'task-description';
+    element.id = task.id;
+    element.appendChild(text);
+    element.appendChild(desc);
+    taskList.appendChild(element);
+}
 
 function postAjax(endpoint, data) {
     var url = urlBase + endpoint;
     console.log('ajax GET - ' + url);
-    var httpRequest = new XMLHttpRequest();
+    httpRequest = new XMLHttpRequest();
 
     if (!httpRequest) {
         alert('Giving up :( Cannot create an XMLHTTP instance');
         return false;
     }
-    var params = typeof data == 'string' ? data : Object.keys(data).map(
-            function(k){ return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]) }
-        ).join('&');
+    // var params = typeof data == 'string' ? data : Object.keys(data).map(
+    //         function(k){ return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]) }
+    //     ).join('&');
 
     httpRequest.onreadystatechange = postCallback;
     
     httpRequest.open('POST', url);
-    httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    httpRequest.send(params);
+    httpRequest.setRequestHeader('Content-Type', 'application/json; charset=UTF-8;');
+    httpRequest.send(JSON.stringify(data));
 }
 
 
@@ -72,14 +99,16 @@ function postCallback() {
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
         console.log('request done - status ', httpRequest.status);
         if (httpRequest.status === 200) {
-            console.log(httpRequest.responseText);
-            resultSpan.innerHTML = httpRequest.responseText;
-            return httpRequest.responseText;
+            debugger;
+            console.log('POST TASK callback', httpRequest.responseText);
+            console.log('POST TASK callback data', httpRequest.responseData);
+            // resultSpan.innerHTML = httpRequest.responseText;
+            // return httpRequest.responseText;
         } else {
             alert('There was a problem with the request.');
-            console.error('ERROR with the request ', httpRequest.response);
+            console.error('ERROR with the POST request ', httpRequest.response);
         }
     } else {
-        console.log('request not done ', httpRequest.readyState);
+        console.log('POST request not done ', httpRequest.readyState);
     }
 }
---
+++ promises.js
@@ -0,0 +1,9 @@
+function getAjaxPromise(url) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
+        xhr.onload = () => resolve(xhr.responseText);
+        xhr.onerror = () => reject(xhr.statusText);
+        xhr.send();
+    });
+}
